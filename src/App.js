365d8740cb05f0ef5e455596f014a3a7106ae94f
William Dahl <williamad90@gmail.com>
2021-01-09 16:33:37 +0100

    created a simple useState which uses a button to toggel between two texts

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import Setup from './setupFiles/error-example'
+import Setup from './setupFiles/simple-usestate-example'
 
 function App() {
   return (
--- src/setupFiles/simple-usestate-example.js
+++ src/setupFiles/simple-usestate-example.js
@@ -5,23 +5,28 @@ import React, {useState} from 'react';
 * the React fragment allows me to drop a list of children in the return.
 * multiple divs etc. Another syntax for the fragment is empty tags: <></>.
 * */
-export const ErrorExample = () => {
+export const UseStateExample = () => {
     // let title = 'hello from stables'
     // const value = useState(1)[0];
     // const handler = useState(1)[1];
     // console.log(value, handler)
 
+    const [text,setText] = useState('hello from stables');
     const clickHandler = () => {
-        title = 'i love horses';
-        console.log(title);
-    }
+        if(text === 'hello from stables'){
+            setText('i love horses');
+        }
+        else{
+            setText('hello from stables');
+        }
+    };
     return (
         <React.Fragment>
-            <button type='button' className ='btn' onClick={clickHandler}>
-                change title
+            <h3>{text}</h3>
+            <button className = 'btn' onClick={clickHandler}>
+                <h3>click me please</h3>
             </button>
-            <h3>{title}</h3>
         </React.Fragment>
     )
 }
-export default ErrorExample
+export default UseStateExample
