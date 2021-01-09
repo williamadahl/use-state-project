e9b38b7c195871572bae3235e0d5637b9af8eab4
William Dahl <williamad90@gmail.com>
2021-01-09 16:20:41 +0100

    tried out fragments, and accessing the useState() function and its array

--- src/setupFiles/error-example.js
+++ src/setupFiles/error-example.js
@@ -1,8 +1,27 @@
-import React from 'react';
+import React, {useState} from 'react';
 
+
+/*
+* the React fragment allows me to drop a list of children in the return.
+* multiple divs etc. Another syntax for the fragment is empty tags: <></>.
+* */
 export const ErrorExample = () => {
+    // let title = 'hello from stables'
+    // const value = useState(1)[0];
+    // const handler = useState(1)[1];
+    // console.log(value, handler)
+
+    const clickHandler = () => {
+        title = 'i love horses';
+        console.log(title);
+    }
     return (
-        <h3> hello from error</h3>
+        <React.Fragment>
+            <button type='button' className ='btn' onClick={clickHandler}>
+                change title
+            </button>
+            <h3>{title}</h3>
+        </React.Fragment>
     )
 }
 export default ErrorExample
