d0482af9f031cceb7779d6c74ef381354e046c0a
William Dahl <williamad90@gmail.com>
2021-01-11 19:35:10 +0100

    created a more complex counter using functional update with a timeout for updating the prevState value

--- src/setupFiles/use-state-counter-example.js
+++ src/setupFiles/use-state-counter-example.js
@@ -5,6 +5,13 @@ const CounterExample = () => {
     const resetCounter = () => {
         setValue(0);
     };
+    const complexIncrease = () => {
+        setTimeout(() =>{
+            setValue((prevState) => {
+                return prevState + 1;
+            })
+        },2000);
+    };
     return(
         <>
             <section style={{margin: '4rem 0'}}>
@@ -13,6 +20,9 @@ const CounterExample = () => {
                 <button className = 'btn' onClick = {() => setValue(value - 1)}> Decrease </button>
                 <button className = 'btn' onClick = {resetCounter}> Reset </button>
                 <button className = 'btn' onClick = {() => setValue(value + 1)}> Increase </button>
+                <h3> complex counter </h3>
+                <h3> {value} </h3>
+                <button className = 'btn' onClick = {() => complexIncrease()}> Increase </button>
             </section>
         </>
     );
