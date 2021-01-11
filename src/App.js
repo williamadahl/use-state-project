ec00368757618176df02588379e7c860bcf5108f
William Dahl <williamad90@gmail.com>
2021-01-11 19:20:01 +0100

    created a simple counter where value is increased / decreased and reset with useState handlers

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import './App.css';
 import SimpleUseStateExapmle from './setupFiles/simple-usestate-example'
 import UseStateArray from './setupFiles/use-state-array-example'
 import UseStateObject from './setupFiles/use-state-object-example'
+import UseStateCounter from './setupFiles/use-state-counter-example'
 
 function App() {
   return (
       <div className = 'container'>
-      <UseStateObject />
+      <UseStateCounter />
       </div>
   );
 }
---
+++ src/setupFiles/use-state-counter-example.js
@@ -0,0 +1,20 @@
+import React, {useState} from 'react';
+
+const CounterExample = () => {
+    const [value, setValue] = useState(0);
+    const resetCounter = () => {
+        setValue(0);
+    };
+    return(
+        <>
+            <section style={{margin: '4rem 0'}}>
+                <h3> regular counter </h3>
+                <h3> {value} </h3>
+                <button className = 'btn' onClick = {() => setValue(value - 1)}> Decrease </button>
+                <button className = 'btn' onClick = {resetCounter}> Reset </button>
+                <button className = 'btn' onClick = {() => setValue(value + 1)}> Increase </button>
+            </section>
+        </>
+    );
+}
+export default CounterExample;
