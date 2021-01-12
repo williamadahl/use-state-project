5c8cdc020c4daaea533aedd743fc99ad9bda4363
William Dahl <williamad90@gmail.com>
2021-01-12 21:45:11 +0100

    simple useEffect with changing document title per re-render

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import SimpleUseStateExapmle from './setupFiles/simple-usestate-example'
 import UseStateArray from './setupFiles/use-state-array-example'
 import UseStateObject from './setupFiles/use-state-object-example'
 import UseStateCounter from './setupFiles/use-state-counter-example'
+import UseEffectBasic from './useEffect/useEffect-basic'
+
 
 function App() {
   return (
       <div className = 'container'>
-      <UseStateCounter />
+      <UseEffectBasic />
       </div>
   );
 }
---
+++ src/useEffect/useEffect-basic.js
@@ -0,0 +1,22 @@
+import React, {useState, useEffect} from 'react';
+// UseEffect reruns after every re-render by default
+// cleanup function
+// second parameter
+
+const UseEffectBasic = () => {
+    const [value, setValue] = useState(0);
+    useEffect(() => {
+        console.log('call useEffect()');
+        document.title = `Clicked me ${value}`;
+    });
+    return(
+        <>
+            <h3>{value}</h3>
+            <button className = 'btn' onClick={() => setValue(value+1)}>
+                click me harder!!
+            </button>
+        </>
+
+    );
+}
+export default UseEffectBasic;
