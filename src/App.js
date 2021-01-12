6c4cea46e39ea9a06836726aebeccd9dfe2761ce
William Dahl <williamad90@gmail.com>
2021-01-12 23:05:11 +0100

    Created a simple exapmle with useEffect rendering data fetched with an async await. object destructurinrg and map function was used to display object values correctly

--- src/App.js
+++ src/App.js
@@ -5,12 +5,12 @@ import UseStateObject from './setupFiles/use-state-object-example'
 import UseStateCounter from './setupFiles/use-state-counter-example'
 import UseEffectBasic from './useEffect/useEffect-basic'
 import UseEffectCleanup from './useEffect/useEffect-cleanup'
-
+import UseEffectFetchData from './useEffect/use-effect-fetch-users'
 
 function App() {
   return (
       <div className = 'container'>
-      <UseEffectCleanup />
+      <UseEffectFetchData />
       </div>
   );
 }
