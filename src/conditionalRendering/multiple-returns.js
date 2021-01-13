eb4ab8fe5812845dd950d2037f72dab49344ff78
William Dahl <williamad90@gmail.com>
2021-01-13 19:52:38 +0100

    added an alternative way to set up fetch, response and error ipossing return values in arrows

--- src/conditionalRendering/multiple-returns.js
+++ src/conditionalRendering/multiple-returns.js
@@ -6,6 +6,7 @@ const MultipleReturns = () => {
     const [isLoading, setIsLoading] = useState(true);
     const [isError, setIsError] = useState(false);
     const [user, setUser] = useState('default user');
+    /*
     const getUser = async () =>{
         const response = await fetch(url);
         if(response.status >= 200 && response.status <= 299){
@@ -24,6 +25,25 @@ const MultipleReturns = () => {
     useEffect(() => {
         getUser();
     }, [tmp]);
+    */
+    useEffect(()=> {
+        fetch(url)
+            .then((resp) => {
+                if (resp.status >= 200 && resp.status <= 299) {
+                    return resp.json();
+                } else {
+                    setIsLoading(false);
+                    setIsError(true);
+                    throw new Error(resp.statusText);
+                }
+            }).then((user) => {
+            const {login} = user;
+            setUser(login);
+            setIsLoading(false);
+        }).catch((error) => {
+            console.log(error);
+        });
+    },[]);
 
     if(isLoading){
         return (<> <h1>Loading...</h1> </>);
