38124e71de44ece2e1c53f98d5db282b45ea3486
William Dahl <williamad90@gmail.com>
2021-01-13 20:55:47 +0100

    made a simple toggle button using the short-circuiit evaluation logic

--- src/conditionalRendering/short-circuit-eval.js
+++ src/conditionalRendering/short-circuit-eval.js
@@ -9,17 +9,21 @@ import React, {useState, useEffect} from 'react';
 
 
 const ShortCircuit = () =>{
-    const [text, setText] = useState('..');
-    const firstValue = text || ' let me whip you.';
-    const secondValue = text && ' let me sell you.';
+    const [text, setText] = useState('');
+    const [isError, setIsError] = useState(false);
+    // const firstValue = text || ' let me whip you.';
+    // const secondValue = text && ' let me sell you.';
 
     return (
       <>
-          <h1>First: {firstValue}</h1>
-          <h1>Second: {secondValue}</h1>
-          {text || <h3>or operator</h3>}
-          {text && <h3>and operator</h3>}
-          {!text && <h3> not and operator</h3>}
+          {/*<h1>First: {firstValue}</h1> */}
+          {/* <h1>Second: {secondValue}</h1>*/}
+          {/* {text || <h3>or operator</h3>}*/}
+          {/* {text && <h3>and operator</h3>}*/}
+          {/* {!text && <h3> not and operator</h3>}*/}
+          <h1>Toggle example</h1>
+          <button className = 'btn' onClick={() => setIsError(!isError)}> Toggle error </button>
+          {isError && <h3>Error...</h3>}
       </>
     );
 
