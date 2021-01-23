4fc4d4a12d91014aa08b0ec52c9cf80a9d0b392d
William Dahl <williamad90@gmail.com>
2021-01-23 20:39:25 +0100

    added a cart that WOULD make all components re-render when counter is clickerd. With useCallback() we bypass, and ONLY re-render compenents if that function is called. That means that the other state values are untouched and will NOT trigger a re-render

--- src/useMemo/index.js
+++ src/useMemo/index.js
@@ -1,31 +1,39 @@
 import React, {useState, useCallback, useMemo} from 'react';
 import {useFetch} from '../customHooks/useFetch';
-
 const url = 'https://course-api.com/javascript-store-products';
+
+/* Every time a prop or state changes, components re-render! */
+
 const UseMemo = () => {
     const {products} = useFetch(url);
     const [count, setCount] = useState(0);
+    const [cart, setCart] = useState(0);
+
+    const addToCart = useCallback(() => {
+        setCart(cart+1);
+    },[cart]);
 
     return (
         <>
             <h2> Count : {count}</h2>
+            <h2 style={{marginTop:'3rem'}}>Cart : {cart}</h2>
             <button className='btn' onClick={() => setCount(count+1)}> Click me </button>
-            <BigList products={products}/>
+            <BigList products={products} addToCart={addToCart}/>
         </>
     );
 };
 
-const BigList = React.memo(({products}) =>{
+const BigList = React.memo(({products, addToCart}) =>{
     return (
         <section className='products'>
             {products.map((product) => {
-                return <SingleProduct key={product.id} {...product}></SingleProduct>;
+                return <SingleProduct key={product.id} {...product} addToCart={addToCart}></SingleProduct>;
             })}
         </section>
     );
 });
 
-const SingleProduct = ({fields}) =>{
+const SingleProduct = ({fields, addToCart}) =>{
     let {name, price} = fields;
     price = price/100;
     const image = fields.image[0].url;
@@ -34,6 +42,7 @@ const SingleProduct = ({fields}) =>{
             <img src={image} alt={name}/>
             <h4>{name}</h4>
             <p>${price}</p>
+            <button onClick={addToCart}>Add to Cart</button>
         </article>
     );
 };
